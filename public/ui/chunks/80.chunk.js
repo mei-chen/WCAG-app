39feca1aafdc351a87d102198ba112af0db08763
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-04-04 14:21:25 -0400

    removed unimportant file from public

--- public/ui/chunks/80.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[80],{1802:function(n,e,t){t(43)({target:"Number",stat:!0},{isInteger:t(541)})}}]);
-//# sourceMappingURL=80.chunk.js.map
--- public/ui/chunks/81.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[81],{1706:function(e,n,a){var p=a(43),r=a(537);p({target:"Number",stat:!0,forced:Number.parseInt!=r},{parseInt:r})}}]);
-//# sourceMappingURL=81.chunk.js.map
